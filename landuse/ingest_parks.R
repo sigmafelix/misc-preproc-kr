f90aad176e01526b736bef37fd6c20d3a787c428
Insang Song <sigmafelix@hotmail.com>
2025-12-28 18:14:07 +0900

    park processing code

--- landuse/ingest_parks.R
+++ landuse/ingest_parks.R
@@ -15,10 +15,12 @@ parks_data <-
 
 unique(parks_data$parktypenm)
 parktypenm_map <-
-  c("total", "urban_natural", "urban_national", "small", "children",
+  c(
+    "total", "urban_natural", "urban_national", "small", "children",
     "neighborhood", "historic", "cultural", "waterfront", "cemetery",
     "sports", "ordinance_province", "urban_agricultural", "hazard_prevention",
-    "ordinance_city")
+    "ordinance_city"
+  )
 parktypenm_koen <-
   data.frame(
     class2 = parktypenm_map,
@@ -33,7 +35,7 @@ sgg_lookup <-
 sgg_lookup_11 <-
   sgg_lookup %>%
   dplyr::select(
-    sido_kr, sigungu_1_kr, base_year, adm2_code
+    sido_kr, sido_en, sigungu_1_kr, sigungu_1_en, base_year, adm2_code
   ) %>%
   dplyr::mutate(
     sido_kr = ifelse(sido_kr == "전라북도", "전북특별자치도", sido_kr)
@@ -43,11 +45,11 @@ joinby <-
   dplyr::join_by(
     sido_kr == sido_kr,
     sigungu_1_kr == sigungu_1_kr,
-    year >= base_year
+    year <= base_year
   )
 
 
-parks_data <-
+parks_data_cl <-
   parks_data %>%
   dplyr::select(-blank) %>%
   dplyr::mutate(
@@ -61,6 +63,10 @@ parks_data <-
   dplyr::mutate(
     sggcd11 = substr(sggcd, 1, 11),
     year = as.integer(substr(year, 1, 4))
+  ) %>%
+  dplyr::rename(
+    parks = n_parks,
+    park_area = area_sqm
   ) %>%
   dplyr::group_by(sggcd11) %>%
   dplyr::mutate(
@@ -71,41 +77,38 @@ parks_data <-
   dplyr::left_join(
     sgg_lookup_11,
     by = joinby
-  )
-
-## pivotting ...
-  dplyr::mutate(
-    unit = "square meters",
-    value = AREA_SQM
   ) %>%
+  dplyr::filter(year %in% c(2010, 2015, 2020)) %>%
+  dplyr::filter(!is.na(adm2_code)) %>%
   dplyr::select(
-    adm2_code = ADM2_CODE,
-    year = DATA_YEAR,
-    type,
-    class1,
-    class2,
+    year,
+    sido_en,
+    sigungu_1_en,
+    adm2_code,
+    type, class1, class2, parks, park_area
+  ) %>%
+  dplyr::rename(
+    adm1 = sido_en,
+    adm2 = sigungu_1_en
+  ) %>%
+  tidyr::pivot_longer(
+    cols = c(parks, park_area),
+    names_to = "variable",
+    values_to = "value"
+  ) %>%
+  dplyr::mutate(
+    adm1_code = as.integer(substr(adm2_code, 1, 2)),
+    adm2_code = as.integer(adm2_code),
+    class2 = paste0(class2, "_", variable),
+    unit = ifelse(variable == "parks", "count", "square meters")
+  ) %>%
+  dplyr::relocate(
+    adm1, adm1_code,
+    adm2, adm2_code,
+    year,
+    type, class1, class2,
     unit,
     value
   )
 
-# check
-pkd_t <-
-  parks_data %>%
-  dplyr::select(-blank) %>%
-  dplyr::mutate(
-    type = "landuse",
-    class1 = "park"
-  ) %>%
-  dplyr::left_join(
-    parktypenm_koen,
-    by = "parktypenm"
-  ) %>%
-  dplyr::mutate(
-    sggcd11 = substr(sggcd, 1, 11),
-    year = as.integer(substr(year, 1, 4))
-  ) %>%
-  dplyr::group_by(sggcd11) %>%
-  dplyr::mutate(
-    sido_kr = dplyr::first(sggnm),
-    sigungu_1_kr = sggnm
-  )
+## TODO: processing logic after 2020 until 2025 census data release
